e99dff082d72d01325594bf5cb2a069c794149bb
hanatawfik8 <hannatawfik35@gmail.com>
2025-02-06 18:40:46 +0200

    Display Rest Categories Recipe

--- src/app/components/display-category/display-category.component.ts
+++ src/app/components/display-category/display-category.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { DisplayCategoryService } from '../../services/displayCategory/display-category.service';
 import { ICategoryFood } from '../../interfaces/icategory-food';
+import { GetRecipeService } from '../../services/getRecipe/get-recipe.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-display-category',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './display-category.component.html',
   styleUrl: './display-category.component.scss'
 })
@@ -12,7 +14,10 @@ export class DisplayCategoryComponent implements OnInit, OnDestroy {
   displayedFood!: ICategoryFood[]
   callSpecificAPI: any
 
-  constructor(private _displayCategoryService: DisplayCategoryService) { }
+  constructor(
+    private _displayCategoryService: DisplayCategoryService,
+    private _getRecipeService: GetRecipeService
+  ) { }
 
   ngOnInit(): void {
     this.callSpecificAPI = this._displayCategoryService.getSpecificCategory(this._displayCategoryService.sentCategory).subscribe({
@@ -27,6 +32,10 @@ export class DisplayCategoryComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.callSpecificAPI.unsubscribe()
+  }
+
+  sendID(id: string) {
+    this._getRecipeService.sentID = id;
   }
 
 }
